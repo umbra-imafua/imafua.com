8cd71adafab784ec492a471fa04768be1df6283c
umbra-imafua <umbra.imafua@gmail.com>
2023-02-08 14:52:36 +0000

    Commit

--- galleryupdate.sh
+++ galleryupdate.sh
@@ -2,42 +2,40 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-> ASSETS/content.html
+> content.html
 
-echo "<!--content-start-->">> ASSETS/content.html
-echo "  " >> ASSETS/content.html
+echo "<!--content-start-->">> content.html
+echo "  " >> content.html
 
-echo "<!--images-->" >> ASSETS/content.html
+echo "<!--images-->" >> content.html
 
 for file in COMPRESSED/*.jpg COMPRESSED/*.svg COMPRESSED/*.SVG; do
   if true; then
     if [[ "$file" == *"xxx"* ]]; then
-      echo "      <div class=\"image\" rel=\"xxx\"><img src=\"COMPRESSED/$(basename "$file")\"></div>" >> ASSETS/content.html
+      echo "      <div class=\"image\" rel=\"xxx\"><img src=\"COMPRESSED/$(basename "$file")\"></div>" >> content.html
     else
-      echo "      <div class=\"image\"><img src=\"COMPRESSED/$(basename "$file")\"></div>" >> ASSETS/content.html
+      echo "      <div class=\"image\"><img src=\"COMPRESSED/$(basename "$file")\"></div>" >> content.html
     fi
   fi
 
 done
 
-echo "<!--videos-->" >> ASSETS/content.html
+echo "<!--videos-->" >> content.html
 
 for file in COMPRESSED/*.webm; do
   if true; then
     if [[ "$file" == *"xxx"* ]]; then
-      echo "      <div class=\"gif\" rel=\"xxx\"><video autoplay loop muted playsinline ><source src=\"COMPRESSED/$(basename  "${file%.*}").mp4\" type=\"video/mp4\" ><source src=\"COMPRESSED/$(basename "$file")\" type=\"video/webm\" ></video></div>" >> ASSETS/content.html
+      echo "      <div class=\"gif\" rel=\"xxx\"><video autoplay loop muted playsinline ><source src=\"COMPRESSED/$(basename  "${file%.*}").mp4\" type=\"video/mp4\" ><source src=\"COMPRESSED/$(basename "$file")\" type=\"video/webm\" ></video></div>" >> content.html
     else
-      echo "      <div class=\"gif\"><video autoplay loop muted playsinline ><source src=\"COMPRESSED/$(basename  "${file%.*}").mp4\" type=\"video/mp4\" ><source src=\"COMPRESSED/$(basename "$file")\" type=\"video/webm\" ></video></div>" >> ASSETS/content.html
+      echo "      <div class=\"gif\"><video autoplay loop muted playsinline ><source src=\"COMPRESSED/$(basename  "${file%.*}").mp4\" type=\"video/mp4\" ><source src=\"COMPRESSED/$(basename "$file")\" type=\"video/webm\" ></video></div>" >> content.html
     fi
   fi
 done
 
 
 
-echo "<!--content-end-->">> ASSETS/content.html
+echo "<!--content-end-->">> content.html
 
-cp gallery.html ASSETS/gallery.backup
+sed -i -e '/<!--content-start-->/,/<!--content-end-->/!b' -e '/<!--content-end-->/!d;r content.html' -e 'd' gallery.html
 
-sed -i -e '/<!--content-start-->/,/<!--content-end-->/!b' -e '/<!--content-end-->/!d;r ASSETS/content.html' -e 'd' gallery.html
-
-
+rm content.html
